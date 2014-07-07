0ed8f4d892a8c0e446d35169b7e0598753450c1a
Ricado13261 <fra13261@uvg.edu.gt>
2014-07-07 13:45:24 -0600

    tecla next para FM y AM

--- Radio/src/Radio.java
+++ Radio/src/Radio.java
@@ -5,12 +5,19 @@ public class Radio {
 	private double estacionFM[];
 	private int contadorFM;
 	private int contadorAM;
+	private int posicionAM;
+	private int posicionFM;
+	private int estacionActualAM;
+	private double estacionActualFM;
 	
 	public Radio(){
 	estacionAM = new int [100];
 	estacionFM = new double [100];
 	contadorFM=0;
 	contadorAM=0;
+	posicionAM= 0;
+	posicionFM= 0;
+	estacionActualAM=0;
 		for(double i=87.9;i<=107.9;i+=0.2){
 			estacionFM[contadorFM]=i;
 			contadorFM++;
@@ -19,6 +26,33 @@ public class Radio {
 			estacionAM[contadorAM]=j;
 			contadorAM++;
 		}
-		
 	}
+	
+	public int SeleccionoAM(){
+		if(posicionAM >108){
+			posicionAM=0;
+			estacionActualAM=estacionAM[posicionAM];
+			posicionAM++;
+			return estacionActualAM;
+		}else{
+			estacionActualAM=estacionAM[posicionAM];
+			posicionAM++;
+			return estacionActualAM;
+		}
+	}
+	
+	public double SeleccionoFM(){
+		if(posicionFM>100){
+			posicionFM=0;
+			estacionActualFM=estacionFM[posicionFM];
+			posicionFM++;
+			return estacionActualFM;
+		}else{
+			estacionActualFM=estacionFM[posicionFM];
+			posicionFM++;
+			return estacionActualFM;	
+		}
+	}
+	
+	
 }
