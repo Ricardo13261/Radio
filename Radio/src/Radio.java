11b7a2b51d656114183f8cd1bfdb031fff2f8061
SamuelMaldonado <leumasmal@hotmail.com>
2014-07-06 21:31:41 -0600

    Agregué el print prueba 3.

--- Radio/src/Radio.java
+++ Radio/src/Radio.java
@@ -3,6 +3,6 @@ public class Radio {
 	public void saludo(){
 		System.out.println("Hola Mundo");
 		System.out.println("pruebita1");
-		System.out.println("prueba2");
+		System.out.println("prueba3");
 	}
 }
